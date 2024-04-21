92db05b7501c863969f050e945bc38154c13c7ef
TranquillouMAN <beney7189@gmail.com>
2024-04-21 03:10:02 +0200

    MotherPhoque

--- Assets/Scripts/Player/Script.cs
+++ Assets/Scripts/Player/Script.cs
@@ -20,13 +20,49 @@ public class Script : MonoBehaviour
 
         float deltaTime = Time.deltaTime;
         if (Input.GetKey(KeyCode.Z))
+        {
             currVelocity.y += _iSpeed;
+            GetComponent<Animator>().SetBool("MoveUp", true);
+            GetComponent<Animator>().SetBool("Idle", false);
+        }
+        if (Input.GetKeyUp(KeyCode.Z))
+        {
+            GetComponent<Animator>().SetBool("Idle", true);
+            GetComponent<Animator>().SetBool("MoveUp", false);
+        }
         if (Input.GetKey(KeyCode.S))
+        {
             currVelocity.y -= _iSpeed;
+            GetComponent<Animator>().SetBool("MoveBottom", true);
+            GetComponent<Animator>().SetBool("Idle", false);
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            GetComponent<Animator>().SetBool("Idle", true);
+            GetComponent<Animator>().SetBool("MoveBottom", false); 
+        }
         if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.Z) == false && Input.GetKey(KeyCode.S) == false)
+        {
             currVelocity.x += _iSpeed;
+            GetComponent<Animator>().SetBool("MoveRight", true);
+            GetComponent<Animator>().SetBool("Idle", false);
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            GetComponent<Animator>().SetBool("Idle", true);
+            GetComponent<Animator>().SetBool("MoveRight", false);
+        }
         if (Input.GetKey(KeyCode.Q) && Input.GetKey(KeyCode.Z) == false && Input.GetKey(KeyCode.S) == false)
+        {
             currVelocity.x -= _iSpeed;
+            GetComponent<Animator>().SetBool("MoveLeft", true);
+            GetComponent<Animator>().SetBool("Idle", false);
+        }
+        if (Input.GetKeyUp(KeyCode.Q))
+        {
+            GetComponent<Animator>().SetBool("Idle", true);
+            GetComponent<Animator>().SetBool("MoveLeft", false);
+        }
 
 
         GetComponent<Rigidbody2D>().velocity = currVelocity;
