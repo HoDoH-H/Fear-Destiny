27f5acb2a054d3ec02330ae3776e6e0234575c06
HoDoH-H <axelmichon.pro@gmail.com>
2024-06-16 01:32:38 +0200

    Add interior scenes

--- Assets/Scripts/Game Managers/DoorManager.cs
+++ Assets/Scripts/Game Managers/DoorManager.cs
@@ -1,24 +1,43 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class DoorManager : MonoBehaviour
 {
     public Animator anim;
-    public bool canBeInteracted;
+    public bool canBeInteracted = false;
 
     private PlayerController player;
 
+    private void Start()
+    {
+        canBeInteracted = false;
+        player = null;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        canBeInteracted = collision.CompareTag("Player");
-        player = collision.GetComponent<PlayerController>();
+        StartCoroutine(SetInteractable(collision));
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        canBeInteracted = !collision.CompareTag("Player");
+        canBeInteracted = !collision.GetComponent<Character>();
         player = null;
+    }
+
+    IEnumerator SetInteractable(Collider2D collision)
+    {
+        if (collision.GetComponent<PlayerController>())
+        {
+            player = collision.GetComponent<PlayerController>();
+            yield return new WaitUntil(() => !player.Character.IsMoving);
+            yield return new WaitForSeconds(0.1f);
+            canBeInteracted = true;
+        }
+        else
+            canBeInteracted = false;
     }
 
     private void FixedUpdate()
@@ -27,8 +46,16 @@ public class DoorManager : MonoBehaviour
         {
             if (player.input.y > 0 && canBeInteracted)
             {
-                anim.SetTrigger("Open");
+                canBeInteracted = false;
+                StartCoroutine(EnterHouse());
             }
         }
+    }
+
+    IEnumerator EnterHouse()
+    {
+        anim.SetTrigger("Open");
+        yield return new WaitForSeconds(0.5f);
+        this.GetComponent<Portal>().OnPlayerTriggered(player);
     }
 }
