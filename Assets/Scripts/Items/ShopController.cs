3772bc5db17ca645de8a83eded1ea897970d5f63
HoDoH-H <144995071+HoDoH-H@users.noreply.github.com>
2024-10-18 10:06:25 +0200

    Add a system for moves to have a random stat boost

--- Assets/Scripts/Items/ShopController.cs
+++ Assets/Scripts/Items/ShopController.cs
@@ -47,7 +47,9 @@ public class ShopController : MonoBehaviour
 
         if (selectedChoice == 0)
         {
-            // Buy
+            // TODO - Buy (Get rid of what's under before doing business)
+            OnFinish?.Invoke();
+            yield break;
         }
         else if (selectedChoice == 1)
         {
